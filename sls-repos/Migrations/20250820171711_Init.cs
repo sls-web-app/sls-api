1da671a34b806ae880a085d916e4db3d44efd215
TheN3RO <58180782+TheN3RO@users.noreply.github.com>
2025-08-20 19:19:09 +0200

    Clear Migrations

--- sls-repos/Migrations/20250820171711_Init.cs
+++ sls-repos/Migrations/20250820171711_Init.cs
@@ -51,7 +51,7 @@ namespace sls_repos.Migrations
                     Name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
                     Short = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
                     Address = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
-                    Img = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    Img = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: true),
                     CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
                 },
                 constraints: table =>
@@ -64,11 +64,7 @@ namespace sls_repos.Migrations
                 columns: table => new
                 {
                     Id = table.Column<Guid>(type: "uuid", nullable: false),
-                    Email = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
-                    Name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
-                    Surname = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
-                    Role = table.Column<int>(type: "integer", nullable: false),
-                    TeamId = table.Column<Guid>(type: "uuid", nullable: false)
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false)
                 },
                 constraints: table =>
                 {
@@ -87,7 +83,8 @@ namespace sls_repos.Migrations
                     Name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
                     Surname = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
                     ClassName = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: true),
-                    Role = table.Column<int>(type: "integer", nullable: false)
+                    Role = table.Column<int>(type: "integer", nullable: false),
+                    AccountActivated = table.Column<bool>(type: "boolean", nullable: false)
                 },
                 constraints: table =>
                 {
@@ -221,12 +218,6 @@ namespace sls_repos.Migrations
                 name: "IX_Tournaments_EditionId",
                 table: "Tournaments",
                 column: "EditionId");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_UserInvites_Email",
-                table: "UserInvites",
-                column: "Email",
-                unique: true);
 
             migrationBuilder.CreateIndex(
                 name: "IX_Users_Email",
